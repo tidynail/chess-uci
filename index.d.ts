6c5521e01346ed87f3f542ccf2cc29e4d778fa8c
Young-Jo Song <tidynail@gmail.com>
2022-11-18 11:24:42 -0500

    remove d.ts file

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-export = Engine;
-
-declare class Engine {
-  static async start(path: string, 
-    log?: boolean = false, log_recv?: boolean = true, log_send: boolean = true): Engine;
-
-  constructor(path: string, 
-    log?: boolean = false, log_recv?: boolean = true, log_send: boolean = true);
-}
